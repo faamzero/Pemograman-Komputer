bd1d5778cbc7c3c75456abe2466e0f783e562c83
Fakhrul Anam <57746074+faamzero@users.noreply.github.com>
2020-05-01 15:04:14 +0700

    praktikum

---
+++ Pertemuan 8/P07-BigData/src/mapreduce/Koneksi.java
@@ -0,0 +1,24 @@
+
+package mapreduce;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+/**
+ *
+ * @author FaAmZero
+ */
+
+public class Koneksi {
+
+    public static MongoDatabase sambungDB() {
+        try {
+            MongoClient client = MongoClients.create();
+            MongoDatabase database = client.getDatabase("bigdata");
+            return database;
+        } catch (Exception e) {
+            System.out.println("Koneksi Gagal: " + e.getMessage());
+        }
+        return null;
+    }
+}
